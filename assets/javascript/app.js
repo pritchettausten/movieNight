2932fc7c0d6c9666d601141cf67aca8eacb3fc7b
Austen Pritchett <austenpritchett@Austens-MacBook-Air.local>
2017-11-09 23:55:06 -0700

    probably final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+//taco search
+//=============================================================================
+
 var queryURL = "http://taco-randomizer.herokuapp.com/random/?full-taco=true"
 
 $("#tacoButton").on("click", function() {
@@ -8,7 +11,7 @@ $("#tacoButton").on("click", function() {
 
     	event.preventDefault();
 
-		
+//formating the JSON to be readable on the html
 
 		$("#recipe").html("<pre style='word-wrap: break-word; white-space: pre-wrap;'>");
 		$("pre").append(response.recipe);
@@ -21,79 +24,75 @@ $("#tacoButton").on("click", function() {
 		$("pre").append("<br>")
 		$("pre").append("<br>")
 		$("pre").append(response.mixin.recipe);
-
 	});
 });
 
-$.ajax({
-        url: "https://api.themoviedb.org/3/genre/movie/list?api_key=66d2f01d5d725968495c8ffdb6e13ab7&language=en-US", 
-        method: 'GET'
-        }).done(function(response){
-            
-        });
+//popular button search to find random movie
+//=============================================================================
 
-    $("#popularButton").on("click", function () {
-        event.preventDefault();
-        $("#movieSuggestion").empty();
-        $("#rating").empty();
-        
+$("#popularButton").on("click", function () {
+    event.preventDefault();
+    $("#movieSuggestion").empty();
+    $("#rating").empty();
+    
+	var apiKey = "66d2f01d5d725968495c8ffdb6e13ab7"
+	var pageNumber = Math.floor(Math.random() * 50) + 1;
+	var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + pageNumber;
 
-    var apiKey = "66d2f01d5d725968495c8ffdb6e13ab7"
+    $.ajax({
+    url: queryURL, 
+    method: 'GET'
+    }).done(function(response){
+    	console.log(response);
+        var randomNum = Math.floor(Math.random() * 19);
+        var posterURL = "https://image.tmdb.org/t/p/w500"
+        var imageTag = $("<img>").attr("src", posterURL + response.results[randomNum].poster_path);
 
-    var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+//creates new div for the movie information to print on html page
 
+        var greatDiv = $("<div>");
+                var newImg = $("<img>").attr("src", posterURL + response.results[randomNum].poster_path);
+                	newImg.attr("style", "width: 342px;", "height: auto;")    
+            	var paragraphOne = $("<h1>").text(response.results[randomNum].title);
+                var paragraphTwo = $("<p>").text(response.results[randomNum].overview);
+                var paragraphThree = $("<p>").text("Average Rating: " + response.results[randomNum].vote_average);
+                var gap = $("<p>").text(" ");
+                greatDiv.append(paragraphOne);
+                greatDiv.append(newImg);
+                greatDiv.append(gap);
+                // greatDiv.append(paragraphTwo);
+                greatDiv.append(paragraphThree);
 
-        $.ajax({
-        url: queryURL, 
-        method: 'GET'
-        }).done(function(response){
+               $("#movieSuggestion").append(greatDiv);
+		
+//ajax call for seperate plot and rating
 
-           
-
-            var randomNum = Math.floor(Math.random() * 19);
-            var posterURL = "https://image.tmdb.org/t/p/w500"
-            var imageTag = $("<img>").attr("src", posterURL + response.results[randomNum].poster_path);
-
-
-            var greatDiv = $("<div>");
-                    var newImg = $("<img>").attr("src", posterURL + response.results[randomNum].poster_path);    
-                	var paragraphOne = $("<h1>").text(response.results[randomNum].title);
-                    var paragraphTwo = $("<p>").text(response.results[randomNum].overview);
-                    var paragraphThree = $("<p>").text("Average Rating: " + response.results[randomNum].vote_average);
-                    var gap = $("<p>").text(" ");
-                    greatDiv.append(paragraphOne);
-                    greatDiv.append(newImg);
-                    greatDiv.append(gap);
-                    // greatDiv.append(paragraphTwo);
-                    greatDiv.append(paragraphThree);
-
-                   $("#movieSuggestion").append(greatDiv);
-    		
-    		var userInput = response.results[randomNum].title;
-    		var otherURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=full&apikey=40e9cece";        
-			
-			$.ajax({
-			            url: otherURL,
-			            method: "GET"
-			        }).done(function(response) {
-			            
-			            console.log(response);
-			            var rating = response.Rated;
-			            var story = response.Plot;
-			            var that = $("<p>").text(story);
-			            $("#rating").append(that);
-			            var it = $("<p>").text(rating);
-	                	$("#rating").append(it);
-
-			            
-			        });
-            
-        })
-
+		var userInput = response.results[randomNum].title;
+		var otherURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=full&apikey=40e9cece";        
+		
+		$.ajax({
+		            url: otherURL,
+		            method: "GET"
+		        }).done(function(response) {
+		            
+		            console.log(response);
+		            var rating = response.Rated;
+		            var story = response.Plot;
+		            var that = $("<p>").text(story);
+		            $("#rating").append(that);
+		            var it = $("<p>").text(rating);
+		            	it.attr("style", "text-align: center;");
+                	$("#rating").append(it);
+		        });
+    })
 });
+
+//search for a movie similar to whatever you want
+//=============================================================================
 
 var possibles = [];
 var rating;
+
 $("#movieButton").on("click", function () {
 	event.preventDefault();
 	$("#movieSuggestion").empty();
@@ -101,21 +100,26 @@ $("#movieButton").on("click", function () {
 	var userInput = $("#input1").val().trim();
 	$("#input1").val("");
 	
+//gets id of user input movie
+
 	var queryUrl = "https://api.themoviedb.org/3/search/movie?api_key=66d2f01d5d725968495c8ffdb6e13ab7&query=" + userInput;
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).done(function(response) {
-            // console.log(response);
-            var jack = response.results[0].id
-            
-            var similarUrl = "https://api.themoviedb.org/3/movie/" + jack + "/similar?api_key=66d2f01d5d725968495c8ffdb6e13ab7&language=en-US&page=1";
+            var jack = response.results[0].id;
+            // var pageNumber = Math.floor(Math.random() * 10);
+            var similarUrl = "https://api.themoviedb.org/3/movie/" + jack + "/similar?api_key=66d2f01d5d725968495c8ffdb6e13ab7&language=en-US&page=" + response.total_pages;
+
+//uses the id of the movie to get similar movies
 
             $.ajax({
                 url: similarUrl,
                 method: "GET"
             }).done(function(response) {
-                
+                console.log(similarUrl);
+//makes an array of divs from the list of similar movies
+
                 for (var i = 0; i < 20; i++) {
                     var posterSource = "https://image.tmdb.org/t/p/w342";
                     var greatDiv = $("<div>");
@@ -130,8 +134,10 @@ $("#movieButton").on("click", function () {
                     greatDiv.append(gap);
                     greatDiv.append(rating);
                     possibles.push(greatDiv);
-                }
-                
+                };
+             
+//displays a random div from the array
+
                 function displayIt () {
 	                var item = possibles[Math.floor(Math.random()*possibles.length)];
 	                
@@ -139,11 +145,13 @@ $("#movieButton").on("click", function () {
 	                $("#movieSuggestion").append(item);
 	                
                 };
+                
                 displayIt ();
+
+//ajax call for seperate plot and rating
 
 			    var search = $(".searchTitle");
 			    var searchTitle = search[0].innerHTML;
-			    
 			    var otherURL = "https://www.omdbapi.com/?t=" + searchTitle + "&y=&plot=full&apikey=40e9cece";        
 
 				$.ajax({
@@ -156,41 +164,42 @@ $("#movieButton").on("click", function () {
 					        $("#rating").append(that);
 				            var rating = response.Rated;
 				            var it = $("<p>").text(rating);
+				            	it.attr("style", "text-align: center;");
 		                	$("#rating").append(it);
 				            
 					        });  
             });
-
         });
-
 });
+//search by genre button
+//=============================================================================
 
 $("#genreButton").on("click", function () {
         event.preventDefault();
         $("#movieSuggestion").empty();
         $("#rating").empty();
-        
+   
+//uses the value given to the genre selected to pull up a random movie from that genre
 
-    var apiKey = "66d2f01d5d725968495c8ffdb6e13ab7"
-    var genreNum = "";
+    var genreNum = $("#genreList").val();   
 
-    genreNum = $("#genreList").val();
-    
-    
+//returns a random page number so that we can get back a wider selection of movies
 
-    var queryURL = "https://api.themoviedb.org/3/genre/"+ genreNum + "/movies?api_key=" + apiKey + "&language=en-US&include_adult=false&sort_by=created_at.asc";
+    var pageNumber = Math.floor(Math.random() * 10) + 1;
+    var queryURL = "https://api.themoviedb.org/3/genre/"+ genreNum + "/movies?api_key=66d2f01d5d725968495c8ffdb6e13ab7&language=en-US&include_adult=false&sort_by=created_at.asc&page=" + pageNumber;
 
         $.ajax({
         url: queryURL, 
         method: 'GET'
         }).done(function(response){
 
-            
+//page loads 20 movies so we just grab a random one
 
             var randomNum = Math.floor(Math.random() * 19);
             var posterURL = "https://image.tmdb.org/t/p/w342"
             var imageTag = $("<img>").attr("src", posterURL + response.results[randomNum].poster_path);
 
+//creates new div for the movie information to print on html page
 
             var greatDiv = $("<div>");
                     var newImg = $("<img>").attr("src", posterURL + response.results[randomNum].poster_path);    
@@ -201,11 +210,12 @@ $("#genreButton").on("click", function () {
                     greatDiv.append(paragraphOne);
                     greatDiv.append(newImg);
                     greatDiv.append(gap);
-                    // greatDiv.append(paragraphTwo);
                     greatDiv.append(paragraphThree);
 
                    $("#movieSuggestion").append(greatDiv);
             
+//ajax call for seperate plot and rating
+
      	var userInput = response.results[randomNum].title;
      	var otherURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=full&apikey=40e9cece";        
 
@@ -219,11 +229,14 @@ $("#genreButton").on("click", function () {
 			        $("#rating").append(that);
 		            var rating = response.Rated;
 		            var it = $("<p>").text(rating);
+		            	it.attr("style", "text-align: center;");
                 	$("#rating").append(it);
-		            
 		        });
         })
 });
+
+//calling firebase
+//=============================================================================
 
 var config = {
     apiKey: "AIzaSyCZe0YaRus25JZ15Mnvg7RFAKgQd8zoTMY",
@@ -235,47 +248,73 @@ var config = {
   };
   firebase.initializeApp(config);
 
+  database = firebase.database();
 
+	var cumulativeRating;
+	var numRating;
+	  database.ref("newwestUsers").on("value", function (snapshot) {
+	      cumulativeRating = snapshot.val().cumulativeRatings;
+	      numRating = snapshot.val().numRatings;
+	      var averageRat = cumulativeRating / numRating;
+	      var averageRating = Math.max( Math.round(averageRat * 10) / 10, 2.8 ).toFixed(1);
+	      console.log(averageRating);
+	      $("#theRating").text("The Average User Rating is: " + averageRating);
 
-$("#signIn").on("click", function  (e) {
-    var email = $("#email").val().trim();
-      var password = $("#password").val().trim();
-      var auth = firebase.auth();
-     
+	});
 
-     var promise = auth.signInWithEmailAndPassword(email, password);
-      promise.catch(console.log(e.message));
-});
+	$(".star").on("click", function () {
+	    var valueOf = $(this).val();
+	    var thisIsNew = +numRating + +1;
+	    var thisIsAlsoNew = +cumulativeRating + +valueOf;
+		    database.ref("newwestUsers").set({
+		        cumulativeRatings: thisIsAlsoNew,
+		        numRatings: thisIsNew
+		    });
+	});	    
+//sign in as exsisting user
 
-$("#createUser").on("click", function  (e) {
-    var email = $("#email").val().trim();
-      var password = $("#password").val().trim();
-      var auth = firebase.auth();
-    
+	$("#signIn").on("click", function  (e) {
+	    var email = $("#email").val().trim();
+	    var password = $("#password").val().trim();
+	    var auth = firebase.auth();
+	    
+	    var promise = auth.signInWithEmailAndPassword(email, password);
+	    	promise.catch(console.log(e.message));
+	});
 
-     var promise = auth.createUserWithEmailAndPassword(email, password);
-      promise.catch(console.log(e.message));
-});
-$("#logout").on("click", function () {
-    firebase.auth().signOut();
-});
+//adds the user to firebase and logs in
 
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(firebaseUser);
-        console.log(firebaseUser.email);
-        $("#thePanel").addClass("hide");
-        $("#logout").removeClass("hide");
-        $("#loginDiv").html("<h4>" + "Welcome: " + firebaseUser.email);
-        $("#logout").removeClass("hide");
-        $("#leave").removeClass("hide");
+	$("#createUser").on("click", function  (e) {
+	    var email = $("#email").val().trim();
+	    var password = $("#password").val().trim();
+	    var auth = firebase.auth();
+	    
+	    var promise = auth.createUserWithEmailAndPassword(email, password);
+	    	promise.catch(console.log(e.message));
+	});
+	$("#logout").on("click", function () {
+	    firebase.auth().signOut();
+	});
 
+//adds a welcome element and a rating section if logged in
 
-    } else {
-        console.log("Not Logged In");
-        $("#thePanel").removeClass("hide");
-        $("#logout").addClass("hide");
-        $("#loginDiv").text("Login");
-        $("#leave").addClass("hide");
-    }
-});
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+	    if (firebaseUser) {
+	        console.log(firebaseUser);
+	        console.log(firebaseUser.email);
+	        $("#thePanel").addClass("hide");
+	        $("#logout").removeClass("hide");
+	        $("#loginDiv").html("<h4>" + "Welcome: " + firebaseUser.email);
+	        $("#logout").removeClass("hide");
+	        $("#leave").removeClass("hide");
+			$("#theStars").removeClass("hide");
+
+	    } else {
+	        console.log("Not Logged In");
+	        $("#thePanel").removeClass("hide");
+	        $("#logout").addClass("hide");
+	        $("#loginDiv").text("Login");
+	        $("#leave").addClass("hide");
+			$("#theStars").addClass("hide");
+	    }
+	});
